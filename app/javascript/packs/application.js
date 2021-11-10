21162a4806a7b3169785e26cc02bef21cdb95eea
Jeremy Blondeau <lebucheronjoe@gmail.com>
2021-11-10 15:15:12 +0100

    refacto

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,5 @@ import 'bootstrap';
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+const button = document.querySelector(".btn .btn-primary")
+console.log(button)
